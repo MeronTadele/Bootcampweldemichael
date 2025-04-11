31d3c691c80293e03251aeac37e8ee3e02983194
merontadele <helenbeyene12@gmail.com>
2025-04-11 14:52:27 -0400

    updated

--- Week1fianacial1/src/main/java/org/example/Main.java
+++ Week1fianacial1/src/main/java/org/example/Main.java
@@ -1,7 +1,36 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        // asking user to give me input for P,R,N
+        System.out.println("how much do you want to invest?");
+        double investedMoney = scanner.nextDouble();
+        System.out.println("how much is your annual rate(in %)");
+        double interestRate = scanner.nextDouble();
+        System.out.println("for how long do you want your loan (in years):");
+        double loanLength = scanner.nextDouble();
+        // calculating my monthly interest rate in decimal
+        double monthlyRate = interestRate / 12 / 100;
+        //my total number in monthly
+        double numberOfMonthly = loanLength * 12;
+
+        //monthly payment calculation P[r(1+r)^n] / [(1+r)^n-1]
+        double monthlyPayment= investedMoney * (Math.abs(monthlyRate*Math.pow(1+monthlyRate,numberOfMonthly))
+                / Math.abs(Math.pow(1+monthlyRate,numberOfMonthly)-1));
+        // total interest paid
+        double totalInterest = (monthlyPayment * numberOfMonthly) - investedMoney;
+        //souf in formatted results
+
+        System.out.printf("your investment of $%.2f at  %.2f%% annual interest for  monthly payment with total interest of %.0f year in:\n",investedMoney,interestRate,loanLength);
+        System.out.printf("Monthly payment:$%.2f \n",monthlyPayment);
+        System.out.printf("total interest paid: $%.2f",totalInterest);
+
+
+
     }
-}
+
+
+    }
